eedd7d61f5f1834d2f1f5f46a3ba5c34f5736961
Aman Saxena <saxenaaman1989@gmail.com>
2025-09-04 17:42:28 +0530

    world news

---
+++ worldnew/src/main/java/com/newsproject/worldnew/WorldNewsApplication.java
@@ -0,0 +1,13 @@
+package com.newsproject.worldnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldNewsApplication.class, args);
+	}
+
+}
---
+++ worldnew/src/test/java/com/newsproject/worldnew/WorldNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsproject.worldnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
